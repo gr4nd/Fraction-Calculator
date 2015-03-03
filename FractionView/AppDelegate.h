8e41ab1f9d9a0448c05518960647bf9e4c89756a
Andrew <grAnd@iMac-Andrew.local>
2015-03-03 02:14:06 +0200

    Initial Commit

---
+++ FractionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FractionView
+//
+//  Created by Andrew on 03.03.15.
+//  Copyright (c) 2015 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FractionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FractionView
+//
+//  Created by Andrew on 03.03.15.
+//  Copyright (c) 2015 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
