ad4f29c6616cb7665fd74160147f7b628181d031
gr4nd <andriy.grygoryev@gmail.com>
2016-06-26 18:27:23 +0300

    Nothing changed since February 2015

--- FractionView/ViewController.h
+++ FractionView/ViewController.h
@@ -9,6 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *numerator1TextField;
+@property (weak, nonatomic) IBOutlet UITextField *denominator1TextField;
+@property (weak, nonatomic) IBOutlet UITextField *numerator2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *denominator2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *resultTextField;
+
+
+- (IBAction)sumButton:(id)sender;
+- (IBAction)subtractButton:(id)sender;
+- (IBAction)multiplyButton:(id)sender;
+- (IBAction)divideButton:(id)sender;
+
 
 
 @end
--- FractionView/ViewController.m
+++ FractionView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Fraction.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,74 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+
+- (IBAction)sumButton:(id)sender {
+    NSLog(@"SumButton pressed");
+    Fraction* f1=[[Fraction alloc] init];
+    f1.numerator=[[self.numerator1TextField text] intValue];
+    f1.denominator=[[self.denominator1TextField text] intValue];
+    [f1 print];
+    Fraction* f2=[[Fraction alloc] init];
+    f2.numerator=[[self.numerator2TextField text] intValue];
+    f2.denominator=[[self.denominator2TextField text] intValue];
+    [f2 print];
+    [f1 add:f2];
+    [f1 print];
+    NSString* result=[NSString stringWithFormat:@"%i/%i",f1.numerator,f1.denominator];
+    self.resultTextField.text=result;
+    
+}
+
+- (IBAction)subtractButton:(id)sender {
+    NSLog(@"subtract Button pressed");
+    Fraction* f1=[[Fraction alloc] init];
+    f1.numerator=[[self.numerator1TextField text] intValue];
+    f1.denominator=[[self.denominator1TextField text] intValue];
+    [f1 print];
+    Fraction* f2=[[Fraction alloc] init];
+    f2.numerator=[[self.numerator2TextField text] intValue];
+    f2.denominator=[[self.denominator2TextField text] intValue];
+    [f2 print];
+    [f1 subtract:f2];
+    [f1 print];
+    NSString* result=[NSString stringWithFormat:@"%i/%i",f1.numerator,f1.denominator];
+    self.resultTextField.text=result;
+}
+
+- (IBAction)multiplyButton:(id)sender {
+    NSLog(@"multiply Button pressed");
+    Fraction* f1=[[Fraction alloc] init];
+    f1.numerator=[[self.numerator1TextField text] intValue];
+    f1.denominator=[[self.denominator1TextField text] intValue];
+    [f1 print];
+    Fraction* f2=[[Fraction alloc] init];
+    f2.numerator=[[self.numerator2TextField text] intValue];
+    f2.denominator=[[self.denominator2TextField text] intValue];
+    [f2 print];
+    [f1 multiply:f2];
+    [f1 print];
+    NSString* result=[NSString stringWithFormat:@"%i/%i",f1.numerator,f1.denominator];
+    self.resultTextField.text=result;
+}
+
+- (IBAction)divideButton:(id)sender {
+    NSLog(@"DivideButton pressed");
+    Fraction* f1=[[Fraction alloc] init];
+    f1.numerator=[[self.numerator1TextField text] intValue];
+    f1.denominator=[[self.denominator1TextField text] intValue];
+    [f1 print];
+    Fraction* f2=[[Fraction alloc] init];
+    f2.numerator=[[self.numerator2TextField text] intValue];
+    f2.denominator=[[self.denominator2TextField text] intValue];
+    [f2 print];
+    [f1 divide:f2];
+    [f1 print];
+    NSString* result=[NSString stringWithFormat:@"%i/%i",f1.numerator,f1.denominator];
+    self.resultTextField.text=result;
 }
 
 @end
